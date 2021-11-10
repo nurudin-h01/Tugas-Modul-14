97c9dbbba7a48c618ef17a8d4a8c0dd0712a5e59
Muhammad Nurudin <muhammadnurudin01@gmail.com>
2021-11-10 10:29:52 +0700

    soal 02 modul 14

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,13 +3,13 @@ import { CardContainer, CardImage, CardBody, CardPrice, CardProductName, CardTit
 import Button from './Button';
 
 
-const Card = () => {
+const Card = (props) => {
   return (
     <CardContainer width="500px">
-      <CardImage src="https://images.unsplash.com/photo-1521369909029-2afed882baee?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80" />
+      <CardImage src={props.data.imageUrl}/>
       <CardBody>
-        <CardPrice>Rp 99000</CardPrice>
-        <CardProductName>Topi Baseball</CardProductName>
+        <CardPrice>{props.data.price}</CardPrice>
+        <CardProductName>{props.data.productName}</CardProductName>
         <Button />
       </CardBody>
     </CardContainer>
